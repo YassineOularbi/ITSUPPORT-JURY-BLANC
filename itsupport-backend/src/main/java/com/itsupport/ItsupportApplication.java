26d627ae2c6084ffdfe7d8d04db237175538ef2b
YassineOularbi <yassineoularbi4@gmail.com>
2024-08-05 12:23:43 +0100

    create application back end spring boot

---
+++ itsupport-backend/src/main/java/com/itsupport/ItsupportApplication.java
@@ -0,0 +1,13 @@
+package com.itsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsupportApplication.class, args);
+	}
+
+}
