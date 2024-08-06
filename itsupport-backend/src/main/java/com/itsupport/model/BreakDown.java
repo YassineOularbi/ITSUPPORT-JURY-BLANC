b22136d97ec3004e29f0f252f4d963d4566b672c
YassineOularbi <yassineoularbi4@gmail.com>
2024-08-06 21:36:59 +0100

    Add Class BreakDown with relations

---
+++ itsupport-backend/src/main/java/com/itsupport/model/BreakDown.java
@@ -0,0 +1,26 @@
+package com.itsupport.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "break_down")
+public class BreakDown {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @ManyToOne
+    @JsonIgnore
+    private Equipment equipment;
+}
